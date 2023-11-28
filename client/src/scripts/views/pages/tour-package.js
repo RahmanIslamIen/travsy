4330505397fd5d35583857918c4e0b457ea6148d
septian20110042 <septianhadiwibowo79@gmail.com>
2023-11-28 18:18:09 +0700

    add home ,tour-packages

--- client/src/scripts/views/pages/tour-package.js
+++ client/src/scripts/views/pages/tour-package.js
@@ -1,6 +1,154 @@
 function TourPackage() {
   return /* html */ `
-    <h2>Halaman Paket Wisata</h2>
+        <div class="max-w-[90rem] mx-auto bg-primary-50">
+            <div class="flex flex-col gap-[4.88rem] justify-start items-center">
+                <div class="flex flex-col justify-start items-center">
+
+                <div class="flex flex-col gap-16 justify-start items-center">
+                    <div class="flex flex-col gap-8 justify-start items-center">
+                        <p class="block w-[78rem] text-5xl font-bold">Destinasi mana yang ingin kamu kunjungi?</p>
+                        <div class="flex flex-col gap-16 justify-start items-center">
+        <div class="flex flex-col gap-8 justify-start items-center">
+            <p class="block w-[78rem] text-5xl font-bold">Destinasi mana yang ingin kamu kunjungi?</p>
+            <div class="flex flex-row gap-2 justify-start items-center w-[78rem]">
+                <div class="flex flex-row gap-2.5 justify-start items-center w-full px-6 py-4 bg-white border-primary-300 border-[2]px rounded-xl">
+                    <input type="text" id="searchInput" placeholder="Cari paket, destinasi, atau tourguide wisata disini" class="w-full text-neutral-400 focus:outline-none" />
+                </div>
+                <button id="searchButton" class="flex flex-row gap-2.5 justify-center items-center w-40 h-full px-6 py-3 bg-primary-500 rounded-xl" onclick="search()">
+                    <p class="block text-white ">Cari</p>
+                </button>
+            </div>
+        </div>
+                    </div>
+                    <div class="flex flex-col gap-8 justify-start items-start">
+                        <div class="flex flex-col gap-8 justify-start items-start w-[58.13rem]">
+                            <p class="block w-full text-[2rem] text-primary-700  font-bold">Paket Terpopuler</p>
+                            <p class="block w-[51.13rem] text-xl text-zinc-500 ">Kami menyajikan beberapa paket wisata menarik yang sering dikunjungi oleh
+                                wisatawan lokal maupun mancanegara.</p>
+                        </div>
+                        <div class="flex flex-row gap-6 justify-center items-start w-[78rem]">
+                            <div class="flex flex-col gap-4 justify-start items-start w-[18.38rem] p-6 bg-white rounded-2xl">
+                                <div class="rounded-lg">
+                                    <img class="block" src="./assets/image-68564.98329767336.png" />
+                                </div>
+                                <div class="flex flex-row gap-2 justify-start items-center">
+                                    <img class="block" src="./assets/image-17427.5926732854.png" />
+                                    <p class="block text-xs text-teal-900 ">Abram Saris</p>
+                                </div>
+                                <p class="block w-full text-teal-900 ">Pura Tanah Lot - Pura Uluwatu - Pantai Kuta</p>
+                                <div class="flex flex-row gap-1 justify-start items-start">
+                                    <div class="flex flex-row gap-2.5 justify-center items-center px-2 py-1 bg-primary-50 rounded-[2.13rem]">
+                                        <p class="block text-[0.50rem] text-primary-800 ">Malioboro</p>
+                                    </div>
+                                    <div class="flex flex-row gap-2.5 justify-center items-center px-2 py-1 bg-primary-50 rounded-[2.13rem]">
+                                        <p class="block text-[0.50rem] text-primary-800 ">Gembira Loka</p>
+                                    </div>
+                                    <div class="flex flex-row gap-2.5 justify-center items-center px-2 py-1 bg-primary-50 rounded-[2.13rem]">
+                                        <p class="block text-[0.50rem] text-primary-800 ">Prambanan</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="flex flex-col gap-4 justify-start items-start w-[18.38rem] p-6 bg-white rounded-2xl">
+                                <div class="rounded-lg">
+                                    <img class="block" src="./assets/image-10476.858624630258.png" />
+                                </div>
+                                <div class="flex flex-row gap-2 justify-start items-center">
+                                    <img class="block" src="./assets/image-46245.661386058346.png" />
+                                    <p class="block text-xs text-teal-900 ">Abram Saris</p>
+                                </div>
+                                <p class="block w-full text-teal-900 ">Pura Tanah Lot - Pura Uluwatu - Pantai Kuta</p>
+                                <div class="flex flex-row gap-1 justify-start items-start">
+                                    <div class="flex flex-row gap-2.5 justify-center items-center px-2 py-1 bg-primary-50 rounded-[2.13rem]">
+                                        <p class="block text-[0.50rem] text-primary-800 ">Malioboro</p>
+                                    </div>
+                                    <div class="flex flex-row gap-2.5 justify-center items-center px-2 py-1 bg-primary-50 rounded-[2.13rem]">
+                                        <p class="block text-[0.50rem] text-primary-800 ">Gembira Loka</p>
+                                    </div>
+                                    <div class="flex flex-row gap-2.5 justify-center items-center px-2 py-1 bg-primary-50 rounded-[2.13rem]">
+                                        <p class="block text-[0.50rem] text-primary-800 ">Prambanan</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="flex flex-col gap-4 justify-start items-start w-[18.38rem] p-6 bg-white rounded-2xl">
+                                <div class="rounded-lg">
+                                    <img class="block" src="./assets/image-6501.406248826691.png" />
+                                </div>
+                                <div class="flex flex-row gap-2 justify-start items-center">
+                                    <img class="block" src="./assets/image-60483.69389353132.png" />
+                                    <p class="block text-xs text-teal-900 ">Abram Saris</p>
+                                </div>
+                                <p class="block w-full text-teal-900 ">Pura Tanah Lot - Pura Uluwatu - Pantai Kuta</p>
+                                <div class="flex flex-row gap-1 justify-start items-start">
+                                    <div class="flex flex-row gap-2.5 justify-center items-center px-2 py-1 bg-primary-50 rounded-[2.13rem]">
+                                        <p class="block text-[0.50rem] text-primary-800 ">Malioboro</p>
+                                    </div>
+                                    <div class="flex flex-row gap-2.5 justify-center items-center px-2 py-1 bg-primary-50 rounded-[2.13rem]">
+                                        <p class="block text-[0.50rem] text-primary-800 ">Gembira Loka</p>
+                                    </div>
+                                    <div class="flex flex-row gap-2.5 justify-center items-center px-2 py-1 bg-primary-50 rounded-[2.13rem]">
+                                        <p class="block text-[0.50rem] text-primary-800 ">Prambanan</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="flex flex-col gap-4 justify-start items-start w-[18.38rem] p-6 bg-white rounded-2xl">
+                                <div class="rounded-lg">
+                                    <img class="block" src="./assets/image-96891.4681334091.png" />
+                                </div>
+                                <div class="flex flex-row gap-2 justify-start items-center">
+                                    <img class="block" src="./assets/image-47069.67001038185.png" />
+                                    <p class="block text-xs text-teal-900 ">Abram Saris</p>
+                                </div>
+                                <p class="block w-full text-teal-900 ">Pura Tanah Lot - Pura Uluwatu - Pantai Kuta</p>
+                                <div class="flex flex-row gap-1 justify-start items-start">
+                                    <div class="flex flex-row gap-2.5 justify-center items-center px-2 py-1 bg-primary-50 rounded-[2.13rem]">
+                                        <p class="block text-[0.50rem] text-primary-800 ">Malioboro</p>
+                                    </div>
+                                    <div class="flex flex-row gap-2.5 justify-center items-center px-2 py-1 bg-primary-50 rounded-[2.13rem]">
+                                        <p class="block text-[0.50rem] text-primary-800 ">Gembira Loka</p>
+                                    </div>
+                                    <div class="flex flex-row gap-2.5 justify-center items-center px-2 py-1 bg-primary-50 rounded-[2.13rem]">
+                                        <p class="block text-[0.50rem] text-primary-800 ">Prambanan</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="flex flex-col gap-2.5 justify-start items-center py-16 bg-primary-500">
+                <div class="flex flex-row gap-16 justify-start items-start w-[78rem]">
+                    <div class="flex flex-col gap-2.5 justify-start items-start w-[36.44rem]">
+                        <div>
+                            <img class="block" src="./assets/image-35979.38848805404.png" />
+                        </div>
+                        <p class="block w-full text-white ">Jelajahi keindahan pulau Bali dengan pilihan destinasi menarik dari kami.
+                            Setiap tempat memiliki cerita dan keunikan tersendiri yang menunggu untuk
+                            Anda temukan.Yuk segera pesan layanan kami.</p>
+                    </div>
+                    <div class="flex flex-col gap-4 justify-start items-start">
+                        <p class="block text-xl text-white ">Tautan</p>
+                        <div class="flex flex-col justify-start items-start">
+                            <p class="block text-white ">Paket Wisata</p>
+                            <p class="block text-white ">Riwayat</p>
+                        </div>
+                    </div>
+                    <div class="flex flex-col gap-4 justify-start items-start">
+                        <p class="block text-xl text-white ">Layanan</p>
+                        <div class="flex flex-col justify-start items-start">
+                            <p class="block text-white ">Tour Guide</p>
+                            <p class="block text-white ">Paket Wisata</p>
+                        </div>
+                    </div>
+                    <div class="flex flex-col gap-4 justify-start items-start">
+                        <p class="block text-xl text-white ">Kontak</p>
+                        <div class="flex flex-col justify-start items-start">
+                            <p class="block text-white ">official@travsy.id</p>
+                            <p class="block text-white ">Sleman, Yogyakarta</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
   `;
 }
 
